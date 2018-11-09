8b88b34b926f6971db684541d4431241686aa4cd
Miguel Zdanovich <miguelangel.zdanovich@personal.com.ar>
2018-11-09 01:11:41 -0300

    Adding relationships between models

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,7 +11,13 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Post::class, 300)->create();
+        factory(App\Post::class, 300)->create()->each(function(App\Post $post){
+            $post->tags()->attach([
+                rand(1,5),
+                rand(6,14),
+                rand(15,20),
+            ]);
+        });
         
     }
 }
