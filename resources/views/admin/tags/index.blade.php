2d7e31e5a6cc0a9d29354e69a31cf82b298b2613
Miguel Zdanovich <miguelangel.zdanovich@personal.com.ar>
2018-11-10 01:17:38 -0300

    Se comleto vista Tags y se instalo Laravel Collerctive 5.7

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-8 offset-md-2">
+                <div class="card mb-3 shadow-sm mb-1 bg-white rounded">
+                    <div class="card-header">
+                        <a href="{{ route('tags.create') }}" class="btn btn-primary float-lg-right">Crear</a>
+                        <h3>Lista de etiquetas</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8 offset-md-2">
+                <div class="card mb-3 shadow-lg mb-5 bg-white rounded">
+                    <div class="card-body">
+                        <table class="table">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Nombre</th>
+                                    <th colspan="3">&nbsp;</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($tags as $tag)
+                                <tr>
+                                    <th scope="row">{{ $tag->id }}</th>
+                                    <td>{{ $tag->name }}</td>
+                                    <td width="10px">
+                                        <a href="{{ route('tags.show',$tag->id) }}" class="btn btn-sm btn-outline-secondary">Ver</a>
+                                    </td>
+                                    <td width="10px">
+                                        <a href="{{ route('tags.edit',$tag->id) }}" class="btn btn-sm btn-outline-primary">Editar</a>
+                                    </td>
+                                    <td width="10px">
+                                        <a href="#" class="btn btn-sm btn-outline-danger">Eliminar</a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{ $tags->render() }}
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
+
--- resources/views/web/posts.blade.php
+++ resources/views/web/posts.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
     <div class="container">
-        <div class="col-auto">
-            <h1>Lista de Posts</h1>
-
-            @foreach ($posts as $post)
-                <div class="card mb-3 shadow p-1 mb-5 bg-white rounded">
-                    <img class="card-img-top img-fluid" src="{{ $post->file }}" alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ $post->name }}</h4>
-                        <p class="card-text">{{ $post->excerpt }}...</p>
-                        <p class="card-text"><small class="text-muted float-lg-right">Publicado el {{ $post->created_at}}</small></p>
-                        <a class="btn btn-light" href="{{ route('post', $post->slug ) }}">Leer más</a>
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <h1>Lista de Artículos</h1>
+                @foreach ($posts as $post)
+                    <div class="card mb-3 shadow p-1 mb-5 bg-white rounded">
+                        <img class="card-img-top img-fluid" src="{{ $post->file }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $post->name }}</h4>
+                            <p class="card-text">{{ $post->excerpt }}...</p>
+                            <p class="card-text"><small class="text-muted float-lg-right">Publicado el {{ $post->created_at}}</small></p>
+                            <a class="btn btn-light" href="{{ route('post', $post->slug ) }}">Leer más</a>
+                        </div>
                     </div>
-                </div>
-            @endforeach
-            {{ $posts->render() }}
+                @endforeach
+                {{ $posts->render() }}
+            </div>
         </div>
     </div>
 @endsection
