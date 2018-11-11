edf1f1a30e6da69fe0a8627bf3ea534ec24dc0d6
Miguel Zdanovich <miguelangel.zdanovich@personal.com.ar>
2018-11-11 03:14:33 -0300

    Se actualizo archivo readme.mr y usuario admin@admin.com para pruebas

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,8 +14,8 @@ class UsersTableSeeder extends Seeder
         factory(App\User::class, 29)->create();
 
         App\User::create([
-            'name' => 'Miguel Zdanovich',
-            'email' => 'miguel.zdanovich@gmail.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
             'password' => bcrypt('123456')
         ]);
     }
